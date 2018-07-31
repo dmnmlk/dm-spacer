04b70beff3d6bf52f59f456b5e1faa04eed11396
dmnmlk <dmnmlk@gmail.com>
2018-07-31 06:49:58 +0200

    images for search

---
+++ src/components/Claim.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="wrapper">
+        <h1 class="claimer">SPACER</h1>
+        <p class="subclaimer">
+            Begin your journey through our amazing galaxy,
+            and discover places you never even heard of.
+        </p>
+        <p class="subclaimer">
+            Type anything space-related to start.
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Claim',
+};
+</script>
+
+<style lang="scss" scoped>
+    #wrapper {
+        background-image: "../heroimage.jpg"
+    }
+</style>
+
